5ed9c89ffdf07508ef17cdf9331cd993624bf5c4
Jauhen <jauhen@gmail.com>
2013-09-14 16:27:49 -0700

    Initial I'm driving page

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -72,7 +72,7 @@ PickMeCtrl.prototype.initialize = function(position) {
   });
 };
 
-PickMeCtrl.prototype.showDestination = function(destination, $http) {
+PickMeCtrl.prototype.showDestination = function(destination) {
   this.destination = destination;
   var bounds = new google.maps.LatLngBounds();
   bounds.extend(this.destination);
@@ -117,8 +117,93 @@ PickMeCtrl.prototype.routesProposals = function(route) {
  * @param $routeParams
  * @constructor
  */
-function IAmDrivingCtrl($scope, $routeParams) {
+function IAmDrivingCtrl($scope, $http) {
+  navigator.geolocation.getCurrentPosition(angular.bind(this, this.initialize));
+  this.map = null;
+  this.geocoder = null;
+  this.myLatlng = null;
+  this.destination = null;
+  this.myMarker = null;
+  this.directionsDisplay;
+  this.directionsService = new google.maps.DirectionsService();
+  this.directionsServiceRequestOptions = {
+    origin: null,
+    destination: null,
+    waypoints: [],
+    optimizeWaypoints: true,
+    travelMode: google.maps.DirectionsTravelMode.DRIVING
+  };
+
+  this.http = $http;
+  this.scope = $scope;
+
+  $scope.go = angular.bind(this, function() {
+    this.geocoder.geocode( { 'address': $scope.destination, 'region': 'us'}, angular.bind(this, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        this.showDestination(results[0].geometry.location);
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    }));
+  });
 };
+
+IAmDrivingCtrl.prototype.initialize = function(position) {
+  this.geocoder = new google.maps.Geocoder();
+  this.myLatlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+  var mapOptions = {
+    center: myLatlng,
+    zoom: 12,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  this.directionsDisplay = new google.maps.DirectionsRenderer();
+  this.map = new google.maps.Map($(".map-canvas").get(0), mapOptions);
+  this.directionsDisplay.setMap(this.map);
+  this.myMarker = new google.maps.Marker({
+    position: this.myLatlng,
+    map: this.map,
+    title: "I'm here"
+  });
+};
+
+IAmDrivingCtrl.prototype.showDestination = function(destination) {
+  this.destination = destination;
+  var bounds = new google.maps.LatLngBounds();
+  bounds.extend(this.destination);
+  bounds.extend(this.myLatlng);
+
+  this.map.fitBounds(bounds);
+
+  var destinationMarker = new google.maps.Marker({
+    map: this.map,
+    position: destination,
+    title: "Destination"
+  });
+
+  this.calculateRoad();
+};
+
+IAmDrivingCtrl.prototype.calculateRoad = function() {
+  this.directionsServiceRequestOptions.origin = this.myLatlng;
+  this.directionsServiceRequestOptions.destination = this.destination;
+
+  this.directionsService.route(this.directionsServiceRequestOptions, angular.bind(this, function(response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      this.directionsDisplay.setDirections(response);
+
+      // For each route, display summary information.
+      var route = response.routes[0].legs[0];
+      var steps = '';
+      for (var i = 0; i < route.steps.length; i++) {
+        steps += route.steps[i].start_point;
+      }
+      console.log(steps);
+    }
+  }));
+
+};
+
+
 
 function ListCtrl($scope, $routeParams) {
 };
