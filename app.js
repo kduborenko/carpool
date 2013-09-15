331ad22bf0c8501a08c0adfe968e3de18dbdbd3f
Kirill Duborenko <k.duborenko@gmail.com>
2013-09-14 18:19:57 -0700

    Basic implementation of login proxy

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
-app.use(express.bodyParser());
+//app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
@@ -40,6 +40,7 @@ app.get('/driver/registerRoute', driver.registerRoute);
 app.get('/driver/pickPassenger/:routeId/:companionId', driver.pickPassenger);
 app.get('/driver/route/:routeId', driver.getRoute);
 app.get('/user/:email', user.getUserInfo);
+app.post('/login', user.login);
 
 // SSL
 var credentials = {
--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,5 @@
 var social = require('./social');
+var http = require('http');
 
 exports.getUserInfo = function (req, res) {
   var email = req.params.email;
@@ -8,4 +9,29 @@ exports.getUserInfo = function (req, res) {
       res.send(userInfo)
     }
   );
+};
+
+exports.login = function (req, res) {
+  var body = "";
+  req.on('data',function (chunk) {
+    body += chunk;
+  }).on('end', function () {
+      var fwd = http.request(
+        {
+          hostname: 'pmcmsq.epam.com',
+          path: '/pmc/login.do',
+          headers: req.headers
+        },
+        function (rs) {
+          console.log(rs.statusCode)
+          res.status(rs.statusCode)
+          rs.on('data',function (chunk) {
+            res.write(chunk)
+          }).on('end', function () {
+              res.send()
+            })
+        }
+      );
+      fwd.end(body);
+    });
 };
