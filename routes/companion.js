d5a3847be01dc996368a82f56cf260776f5e1215
Kirill Duborenko <k.duborenko@gmail.com>
2013-09-14 18:44:23 -0700

    UPSA info is added to routes proposals

--- routes/companion.js
+++ routes/companion.js
@@ -1,4 +1,6 @@
 var companionDB = require('../db/companion');
+var social = require('./social');
+var driverDB = require('../db/driver');
 
 exports.pickMe = function (req, res) {
   var from = JSON.parse(req.query.from);
@@ -15,7 +17,7 @@ exports.pickMe = function (req, res) {
 exports.get = function (req, res) {
   var companionId = req.params.companionId;
   companionDB.byId(companionId, function (err, companion) {
-    if (companion) {
+    function renderResponse(routes) {
       res.send({
         companionId: companion._id,
         username: companion.username,
@@ -28,23 +30,49 @@ exports.get = function (req, res) {
           lat: companion.to[1],
           lon: companion.to[0]
         },
-        routesProposals: companion.routes_proposals ?
-          companion.routes_proposals.map(function (routeId) {
-            return {
-              routeId: routeId,
-              routeUrl: req.protocol + "://" + req.headers.host
-                          + "/driver/route/" + routeId,
-              acceptDriverUrl: req.protocol + "://" + req.headers.host
-                          + "/companion/accept/" + routeId + "/" + companion._id
-            }
-          }) :
-          null
+        routesProposals: routes
       });
+    }
+
+    if (companion) {
+      var routes = companion.routes_proposals ?
+        companion.routes_proposals.map(function (routeId) {
+          return {
+            routeId: routeId,
+            routeUrl: req.protocol + "://" + req.headers.host
+              + "/driver/route/" + routeId,
+            acceptDriverUrl: req.protocol + "://" + req.headers.host
+              + "/companion/accept/" + routeId + "/" + companion._id
+          }
+        }) :
+        null;
+
+      if (routes) {
+        resolveUpsaInfo(0, routes, function (routes) {
+          renderResponse(routes);
+        })
+      } else {
+        renderResponse(routes);
+      }
     } else {
       res.status(404).send();
     }
   });
 };
+
+
+function resolveUpsaInfo(index, routes, cb) {
+  driverDB.byId(routes[index].routeId.toString(), function (err, route) {
+    social.getUserByEmail(route.username, function (userInfo) {
+      routes[index].upsaInfo = userInfo;
+      if (++index < routes.length) {
+        resolveUpsaInfo(index, routes, cb);
+      } else {
+        cb.call(null, routes);
+      }
+    })
+  });
+}
 
 exports.acceptDriver = function (req, res) {
   var routeId = req.params.routeId;
